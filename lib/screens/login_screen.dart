a28cc1c856e2e7a9a80c1d89d1400e47fb7e6566
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-11-17 17:51:14 +0500

    created empty screens, starting login screen.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:esperflow/widgets/my_custom_buttom.dart';
+import 'package:esperflow/widgets/my_text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text('ESPERFLOW'),
+          Text('Donate Life, Save Lives'),
+
+          // email text field
+          MyTextField(),
+
+          // password field
+          MyTextField(),
+
+          // forgot text button
+          TextButton(onPressed: () {}, child: Text('Forgot Password')),
+
+          // login button
+          MyCustomButtom(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/my_custom_buttom.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyCustomButtom extends StatelessWidget {
+  const MyCustomButtom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.symmetric(horizontal: 25),
+      child: Center(
+        child: Text('Login', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
